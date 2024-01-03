05897cf32d738773ddde4c74fbe7d632298a2232
Tarek Sabry <tareksabry444@outlook.com>
2024-01-04 00:18:39 +0200

    Fix compiling issue

--- strapi/src/iosMain/kotlin/com/swensonhe/strapikmm/deeplink/DeepLinkProcessor.kt
+++ strapi/src/iosMain/kotlin/com/swensonhe/strapikmm/deeplink/DeepLinkProcessor.kt
@@ -7,17 +7,19 @@ import kotlin.coroutines.resumeWithException
 
 actual object DeepLinkProcessor {
     @Throws(Throwable::class)
-    actual suspend fun process(url: String) = suspendCancellableCoroutine { cont ->
-        FIRDynamicLinks.dynamicLinks()
-            .handleUniversalLink(NSURL(string = url), completion = { dynamicLink, error ->
-                if (error != null) {
-                    cont.resumeWithException(Throwable("getDynamicLink:onFailure $error"))
-                } else {
-                    val deepLink = dynamicLink?.url
-                    deepLink?.absoluteString?.let {
-                        cont.resumeWith(Result.success(it))
+    actual suspend fun process(url: String): String {
+        return suspendCancellableCoroutine { cont ->
+            FIRDynamicLinks.dynamicLinks()
+                .handleUniversalLink(NSURL(string = url), completion = { dynamicLink, error ->
+                    if (error != null) {
+                        cont.resumeWithException(Throwable("getDynamicLink:onFailure $error"))
+                    } else {
+                        val deepLink = dynamicLink?.url
+                        deepLink?.absoluteString?.let {
+                            cont.resumeWith(Result.success(it))
+                        } ?: cont.resumeWithException(Throwable("Unable to obtain dynamicLink"))
                     }
-                }
-            })
+                })
+        }
     }
 }
