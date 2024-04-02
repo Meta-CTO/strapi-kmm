9ff6fc4f8d4d9143bf99010feba5c4e75bbcff43
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2024-04-02 20:45:25 +0200

    Fix js issues

--- strapi/src/jsMain/kotlin/com/metacto/strapikmm/auth/AuthClient.kt
+++ strapi/src/jsMain/kotlin/com/metacto/strapikmm/auth/AuthClient.kt
@@ -3,7 +3,6 @@ package com.metacto.strapikmm.auth
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.auth.AuthCredential
 import dev.gitlive.firebase.auth.auth
-import dev.gitlive.firebase.firebase
 
 actual class AuthClient : AuthProvider {
 
@@ -12,39 +11,41 @@ actual class AuthClient : AuthProvider {
     override fun signInWithApple(
         onSuccess: (AuthCredential, ProfileMetadata) -> Unit, onFail: (Throwable) -> Unit
     ) {
-        Firebase.auth.js.signInWithPopup(
-            firebase.auth.OAuthProvider("apple.com")
-        ).then {
-            val profile = ProfileMetadata(
-                firstName = it.user?.displayName,
-                lastName = null,
-                email = it.user?.email,
-                phoneNumber = it.user?.phoneNumber,
-                pictureUrl = it.user?.photoURL
-            )
-            onSuccess.invoke(AuthCredential(it.credential!!), profile)
-        }.catch {
-            onFail.invoke(it)
-        }
+        // TODO: check the implementation
+//        Firebase.auth.js.signInWithPopup(
+//            firebase.auth.OAuthProvider("apple.com")
+//        ).then {
+//            val profile = ProfileMetadata(
+//                firstName = it.user?.displayName,
+//                lastName = null,
+//                email = it.user?.email,
+//                phoneNumber = it.user?.phoneNumber,
+//                pictureUrl = it.user?.photoURL
+//            )
+//            onSuccess.invoke(AuthCredential(it.credential!!), profile)
+//        }.catch {
+//            onFail.invoke(it)
+//        }
     }
 
     override fun signInWithGoogle(
         onSuccess: (AuthCredential, ProfileMetadata) -> Unit, onFail: (Throwable) -> Unit
     ) {
-        Firebase.auth.js.signInWithPopup(
-            firebase.auth.GoogleAuthProvider()
-        ).then {
-            val profile = ProfileMetadata(
-                firstName = it.user?.displayName,
-                lastName = null,
-                email = it.user?.email,
-                phoneNumber = it.user?.phoneNumber,
-                pictureUrl = it.user?.photoURL
-            )
-            onSuccess.invoke(AuthCredential(it.credential!!), profile)
-        }.catch {
-            onFail.invoke(it)
-        }
+        // TODO: check the implementation
+//        Firebase.auth.js.signInWithPopup(
+//            firebase.auth.GoogleAuthProvider()
+//        ).then {
+//            val profile = ProfileMetadata(
+//                firstName = it.user?.displayName,
+//                lastName = null,
+//                email = it.user?.email,
+//                phoneNumber = it.user?.phoneNumber,
+//                pictureUrl = it.user?.photoURL
+//            )
+//            onSuccess.invoke(AuthCredential(it.credential!!), profile)
+//        }.catch {
+//            onFail.invoke(it)
+//        }
     }
 
     actual fun setAuthOptions(options: AuthOptions?) {
