17ad9de3b9c7f86e0799212b778d91f8e36c75c9
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2024-04-09 12:47:56 +0200

    try different way

--- strapi/src/iosMain/kotlin/com/metacto/strapikmm/errorhandling/errortype/NetworkExceptions.kt
+++ strapi/src/iosMain/kotlin/com/metacto/strapikmm/errorhandling/errortype/NetworkExceptions.kt
@@ -1,7 +1,25 @@
 package com.metacto.strapikmm.errorhandling.errortype
 
 import com.rickclephas.kmp.nserrorkt.asNSError
+import io.ktor.client.engine.darwin.DarwinHttpRequestException
+import platform.CFNetwork.kCFURLErrorCannotConnectToHost
+import platform.CFNetwork.kCFURLErrorNotConnectedToInternet
 
 actual fun Throwable.isNetworkException(): Boolean {
-    return this.asNSError().domain == "NSURLErrorDomain"
+    return if (this is DarwinHttpRequestException) {
+        this.origin.code.toInt() == kCFURLErrorCannotConnectToHost
+                || this.origin.code.toInt() == kCFURLErrorNotConnectedToInternet
+    } else {
+        isInternetConnectionError(this.toString())
+                || this.asNSError().code.toInt() == kCFURLErrorCannotConnectToHost
+                || this.asNSError().code.toInt() == kCFURLErrorNotConnectedToInternet
+    }
+}
+
+fun isInternetConnectionError(errorMessage: String): Boolean {
+    val codeRegex = "Code=(\\S+)".toRegex()
+    val matchResult = codeRegex.find(errorMessage)
+    val codeValue = matchResult?.groups?.get(1)?.value
+    return codeValue?.toIntOrNull() == kCFURLErrorCannotConnectToHost
+            || codeValue?.toIntOrNull() == kCFURLErrorNotConnectedToInternet
 }
