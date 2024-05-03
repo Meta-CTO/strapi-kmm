c1894734c3176f9eaae7290037cfa15671e9c685
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2024-05-03 17:38:56 +0300

    Fix js

--- strapi/src/jsMain/kotlin/com/metacto/strapikmm/deeplink/ShareLinkGenerator.kt
+++ strapi/src/jsMain/kotlin/com/metacto/strapikmm/deeplink/ShareLinkGenerator.kt
@@ -5,6 +5,7 @@ import com.metacto.strapikmm.deeplink.model.BaseUrl
 actual object ShareLinkGenerator {
     actual suspend fun generateShareLink(
         context: Any?,
+        deepLinkValue: String,
         channel: String?,
         referrerCustomerId: String?,
         referrerName: String?,
