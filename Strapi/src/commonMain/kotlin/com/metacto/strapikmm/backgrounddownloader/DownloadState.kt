5d94080258eaf2342c976b731889937db1a30643
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-03-25 17:31:12 +0200

    Fix identifier issue

--- Strapi/src/commonMain/kotlin/com/metacto/strapikmm/backgrounddownloader/DownloadState.kt
+++ Strapi/src/commonMain/kotlin/com/metacto/strapikmm/backgrounddownloader/DownloadState.kt
@@ -2,6 +2,6 @@ package com.metacto.strapikmm.backgrounddownloader
 
 sealed class DownloadState(val url: String) {
     class Downloading(url: String) : DownloadState(url)
-    class Completed(identifier: String, url: String, val path: String) : DownloadState(url)
+    class Completed(val identifier: String, url: String, val path: String) : DownloadState(url)
     class NotDownloaded(url: String) : DownloadState(url)
 }
